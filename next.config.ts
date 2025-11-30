cb8680b5c31816f673516473013bc25eb1a5fcc8
Adil Ali Lakhair <adilijaz227@gmail.com>
2025-11-30 21:06:14 +0500

    Add basePath for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/MyPortfolio',
   images: {
     unoptimized: true,
     remotePatterns: [
